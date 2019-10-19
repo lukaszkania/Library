412488a451a900e38d1749bb74884bf9a4a35ec9
dev <lukasz.kania1996@gmail.com>
2019-10-19 15:23:30 +0200

    Created Book model

--- library/books/models.py
+++ library/books/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinValueValidator 
 
 # Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100, null=False)
+    published_date = models.CharField(max_length=11, null=False)
+    page_count = models.PositiveIntegerField(validators=[MinValueValidator(1)])
+    language = models.CharField(max_length=10, null=False)
+
+    def __str__(self):
+        return self.title
