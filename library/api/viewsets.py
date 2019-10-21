906de1991941ff74dcd4b1603c4305a5077b9231
dev <lukasz.kania1996@gmail.com>
2019-10-21 20:02:11 +0200

    All works except filters

--- library/api/viewsets.py
+++ library/api/viewsets.py
@@ -2,14 +2,24 @@ from books.models import Book
 from .serializers import BookSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
+from django_filters import rest_framework as filters
+
+class BookFilter(filters.FilterSet):
+    class Meta:
+        model = Book
+        fields = {
+            "title":["icontains"], 
+            "authors":["icontains"],
+            "published_date":["icontains"],
+            "language":["icontains"]
+            }
 
 class BookViewSet(viewsets.ViewSet):
+    filterset_class = BookFilter
+    model = BookSerializer.Meta.model
     def list(self, request):
         queryset = Book.objects.all()
         serializer = BookSerializer(queryset, many=True)
         return Response(serializer.data)
 
 
-# class BookViewSet(viewsets.ModelViewSet):
-#     queryset = Book.objects.all()
-#     serializer_class = BookSerializer
--- library/library/settings.py
+++ library/library/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'author.apps.AuthorConfig', # Added author app
     'industrie.apps.IndustrieConfig', # Added industrie app
     "rest_framework",
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
